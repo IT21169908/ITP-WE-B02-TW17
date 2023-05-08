f1c102655aa5cb96710f928fdae1688aec6acae2
Ameesha - IT21160448 <it21160448@my.sliit.lk>
2023-05-08 23:25:47 +0530

    Report genarate and search

--- visionmate.lk/client/src/models/TreatmentPlan.ts
+++ visionmate.lk/client/src/models/TreatmentPlan.ts
@@ -6,14 +6,16 @@ interface ITreatmentPlan {
     treatmentPlan: string; // The recommended treatment plan for the patient's condition.
     startDate: string;
     endDate: string;
-    patientId?: string;
-    doctorId?: string;
-    diagnosis?: string;
-    medications?: string; // Any medications that have been prescribed for the patient.
-    procedures?: string;
-    instructions?: string;
+    patientId: string;
+    doctorId: string;
+    diagnosis: string;
+    medications: string; // Any medications that have been prescribed for the patient.
+    procedures: string;
+    instructions: string;
     referral: string; // If the patient needs to be referred to a specialist, this field would indicate the name of the specialist and any other relevant information.
-    progressNotes?: string; // Any notes on the patient's progress or changes to the treatment plan.
+    progressNotes: string; // Any notes on the patient's progress or changes to the treatment plan.
+
+    [key: string]: string | number
 }
 
 export default ITreatmentPlan
--- visionmate.lk/client/src/views/dashboard/doctor/treatment-plans/Manage.tsx
+++ visionmate.lk/client/src/views/dashboard/doctor/treatment-plans/Manage.tsx
@@ -1,29 +1,32 @@
+import JsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 import React, { useEffect, useState } from 'react';
-import { Col, message, Popconfirm, Row, Skeleton, Table } from 'antd';
-import type {ColumnsType} from 'antd/es/table';
-import {PageHeader} from "../../../../components/breadcrumbs/DashboardBreadcrumb";
-import { HouseDoor, Pencil, PencilFill, Plus, Trash } from "react-bootstrap-icons";
-import {BorderLessHeading, Main} from "../../../../components/styled-components/styled-containers";
-import {Cards} from "../../../../components/cards/frame/CardFrame";
+import { Button, Col, Input, message, Popconfirm, Row, Skeleton, Table } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import { PageHeader } from "../../../../components/breadcrumbs/DashboardBreadcrumb";
+import { Download, HouseDoor, PencilFill, Plus, Search, Trash } from "react-bootstrap-icons";
+import { BorderLessHeading, Main, TopToolBox } from "../../../../components/styled-components/styled-containers";
+import { Cards } from "../../../../components/cards/frame/CardFrame";
 import { Link, useNavigate } from "react-router-dom";
 import ITreatmentPlan from "../../../../models/TreatmentPlan";
 import { TreatmentPlanService } from "../../../../services/TreatmentPlanService";
+import { getCurrentDateTime } from "../../../../utils/date-time";
 
 interface DataType {
-    key:  string;
+    key: string;
     title: string;
     description: string;
     treatmentPlan: string;
     startDate: string;
     endDate: string;
-    patientId?:  string | undefined;
-    doctorId?:  string | undefined;
-    diagnosis?:  string | undefined;
-    medications?:  string | undefined;
-    procedures?:  string | undefined;
-    instructions?:  string | undefined;
+    patientId?: string | undefined;
+    doctorId?: string | undefined;
+    diagnosis?: string | undefined;
+    medications?: string | undefined;
+    procedures?: string | undefined;
+    instructions?: string | undefined;
     referral: string;
-    progressNotes?:  string | undefined;
+    progressNotes?: string | undefined;
     action: JSX.Element;
 }
 
@@ -46,7 +49,7 @@ const dataTableColumn: ColumnsType<DataType> = [
 
 const BreadcrumbItem = [
     {
-        title: <div className="d-flex align-items-center"><HouseDoor/> &nbsp; Home</div>,
+        title: <div className="d-flex align-items-center"><HouseDoor /> &nbsp; Home</div>,
         href: '/doctor',
     },
     {
@@ -58,49 +61,8 @@ const ManageTreatmentPlans: React.FC = () => {
 
     const navigate = useNavigate();
     const [treatmentPlans, setTreatmentPlans] = useState<ITreatmentPlan[]>([]);
+    const [filteredTreatmentPlans, setFilteredTreatmentPlans] = useState<ITreatmentPlan[]>([]);
     const [tableDataSource, setTableDataSource] = useState<DataType[]>([]);
-
-    useEffect(() => {
-        let isMounted = true;
-
-        async function loadTreatmentPlans() {
-            try {
-                const res = await TreatmentPlanService.getAllTreatmentPlans();
-                if (isMounted) {
-                    setTreatmentPlans(res.data);
-                }
-            } catch (error: any) {
-                console.error(error.response.data);
-            }
-        }
-
-        loadTreatmentPlans();
-        return () => {
-            // TODO unset tableDataSource[]
-            isMounted = false;
-        };
-    }, []);
-
-    useEffect(() => {
-        setTableDataSource(formatDataSource(treatmentPlans));
-    }, [treatmentPlans]);
-
-    const confirmDelete = async (id: string): Promise<void> => {
-        try {
-            const res = await TreatmentPlanService.deleteTreatmentPlan(id);
-            if (res.success) {
-                message.success(`${res.message}`);
-                window.location.reload(); // TODO - remove page reload
-            }
-        } catch (error: any) {
-            message.error(`${ error.response.data.error || error.response.data.message }`);
-            console.log(error.response.data.error);
-        }
-    };
-
-    const cancelDelete = () => {
-        message.error('Delete canceled!');
-    };
 
     const formatDataSource = (treatmentPlans: ITreatmentPlan[]): DataType[] => {
         return treatmentPlans.map((treatmentPlanx) => {
@@ -142,7 +104,7 @@ const ManageTreatmentPlans: React.FC = () => {
                             className="btn btn-sm btn-outline-warning fw-bolder me-1 mt-1"
                             to={`/doctor/treatment-plans/${_id}/edit`}
                         >
-                            <PencilFill/>
+                            <PencilFill />
                         </Link>
                         <Popconfirm
                             title="Are you sure delete this plan?"
@@ -152,13 +114,102 @@ const ManageTreatmentPlans: React.FC = () => {
                             cancelText="No"
                         >
                             <Link className="btn btn-sm btn-outline-danger fw-bolder mt-1" to="#">
-                                <Trash/>
+                                <Trash />
                             </Link>
                         </Popconfirm>
                     </div>
                 ),
             };
         });
+    };
+
+    useEffect(() => {
+        let isMounted = true;
+
+        async function loadTreatmentPlans() {
+            try {
+                const res = await TreatmentPlanService.getAllTreatmentPlans();
+                if (isMounted) {
+                    setTreatmentPlans(res.data);
+                    setFilteredTreatmentPlans(res.data);
+                }
+            } catch (error: any) {
+                console.error(error.response.data);
+            }
+        }
+
+        loadTreatmentPlans();
+        return () => {
+            // TODO unset tableDataSource[]
+            isMounted = false;
+        };
+    }, []);
+
+    useEffect(() => {
+        setTableDataSource(formatDataSource(filteredTreatmentPlans));
+    }, [filteredTreatmentPlans, formatDataSource]);
+
+    const confirmDelete = async (id: string): Promise<void> => {
+        try {
+            const res = await TreatmentPlanService.deleteTreatmentPlan(id);
+            if (res.success) {
+                message.success(`${res.message}`);
+                window.location.reload(); // TODO - remove page reload
+            }
+        } catch (error: any) {
+            message.error(`${error.response.data.error || error.response.data.message}`);
+            console.log(error.response.data.error);
+        }
+    };
+
+    const cancelDelete = () => {
+        message.error('Delete canceled!');
+    };
+
+    const generatePDF = (): void => {
+        const doc = new JsPDF('landscape');
+
+        // Add a title to the document
+        doc.text("Treatment Plans Report", 14, 20);
+
+        // Create a table
+        const tableData = treatmentPlans.map((s) => [
+            s.title,
+            s.description,
+            s.treatmentPlan,
+            s.startDate,
+            s.endDate,
+            s.patientId ?? null,
+            s.doctorId ?? null,
+            s.diagnosis ?? null,
+            s.medications ?? null,
+            s.procedures ?? null,
+            s.instructions ?? null,
+            s.referral ?? null,
+            s.progressNotes ?? null,
+        ]);
+        autoTable(doc, {
+            head: [['Title', 'Description', 'Treatment Plan', 'Start Date', 'End Date', 'Patient Id',
+                'Doctor Id', 'Diagnosis', 'Medications', 'Procedures', 'Instructions', 'Referral', 'Progress Notes']],
+            body: tableData,
+        });
+
+        // Save the document
+        doc.save(`treatment-plans-report-${getCurrentDateTime()}.pdf`);
+    };
+
+    const handleSearch = (e: any) => {
+        console.log(e.target.value);
+        const data = treatmentPlans.filter((item) => {
+            return Object.keys(item).some((key) => {
+                if (item[key]) {
+                    return item[key].toString().toLowerCase().includes(e.target.value.toLowerCase());
+                }
+                return null;
+                }
+            );
+        });
+        setFilteredTreatmentPlans(data);
     };
 
     if (tableDataSource.length === 0) {
@@ -175,15 +226,27 @@ const ManageTreatmentPlans: React.FC = () => {
 
     return (
         <>
-            <PageHeader className="ninjadash-page-header-main" title="Manage Treatment Plans" routes={BreadcrumbItem}/>
+            <PageHeader className="ninjadash-page-header-main" title="Manage Treatment Plans" routes={BreadcrumbItem} />
             <Main>
                 <Row gutter={15}>
                     <Col xs={24}>
+                        <TopToolBox>
+                            <Row gutter={0}>
+                                <Col xxl={7} lg={12} xs={24}>
+                                    <Input suffix={<Search/>} onChange={handleSearch} placeholder="Search Appointments..."/>
+                                </Col>
+                            </Row>
+                        </TopToolBox>
                         <BorderLessHeading>
                             <Cards isbutton={
-                                <Link className="btn btn-primary h-auto" type="link" to="/doctor/treatment-plans/create">
-                                   <Plus/> Add New
-                                </Link>
+                                <>
+                                    <Button className="btn btn-warning h-auto me-2" onClick={generatePDF}>
+                                        <Download className="me-2" /> Generate PDF
+                                    </Button>
+                                    <Link className="btn btn-primary h-auto" type="link" to="/doctor/treatment-plans/create">
+                                        <Plus /> Add New
+                                    </Link>
+                                </>
                             }>
                                 <Table columns={dataTableColumn} dataSource={tableDataSource}/>
                             </Cards>
